94252c117e80d327eb8636462ef638f4c7efeec4
Admin <Admin@Rasco-MacBook.local>
2015-11-07 20:08:03 -0800

    Added Button functionality

--- client.py
+++ client.py
@@ -1,25 +1,33 @@
 from SensorProxy import SensorProxy
 from TemperatureSensor import TemperatureSensor
+from ButtonSensor import ButtonSensor
 from BuzzerActuator import BuzzerActuator
 from HighTemperatureObserver import HighTemperatureObserver
+from ButtonPressedObserver import ButtonPressedObserver
 from Event import Event
 
 # Sensors
 temperatureSensor = TemperatureSensor()
+buttonSensor = ButtonSensor()
 
 # SensorProxies (Initialize with sensor)
-temperatureSensorProxy = SensorProxy(temperatureSensor, 1)
+temperatureSensorProxy = SensorProxy(temperatureSensor, 2)
+buttonSensorProxy = SensorProxy(buttonSensor, 1)
 
 # Actuators
 buzzerActuator=BuzzerActuator()
 
 # Events (Initialize with actuators)
 highTemperatureEvent = Event([buzzerActuator])
+buttonPressedEvent=Event([buzzerActuator])
 
 # Observers (Initialize with proxies they subscribe to and events that should be raised)
-highTemperatureObserver = HighTemperatureObserver(temperatureSensorProxy,10,[highTemperatureEvent])
+highTemperatureObserver = HighTemperatureObserver(temperatureSensorProxy,5,[highTemperatureEvent])
+buttonPressedObserver = ButtonPressedObserver(buttonSensorProxy,[buttonPressedEvent])
 
 # Add Observers
 temperatureSensorProxy.addObserver(highTemperatureObserver)
+buttonSensorProxy.addObserver(buttonPressedObserver)
 
 temperatureSensorProxy.start()
+buttonSensorProxy.start()
