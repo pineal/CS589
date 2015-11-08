49dab97b09102a92e94261bef243f5d25980b026
Admin <Admin@Rasco-MacBook.local>
2015-11-07 18:05:13 -0800

    Cleanup

--- client.py
+++ client.py
@@ -1,28 +1,26 @@
 import SensorProxy
 import TemperatureSensor
-#import BuzzerActuator
+# import BuzzerActuator
 import HighTemperatureEvent
 import HighTemperatureObserver
 
-import threading
-import time 
-
-#Sensors
+# Sensors
 temperatureSensor = TemperatureSensor.TemperatureSensor()
 
-#SensorProxies (Initialize with sensor)
-temperatureSensorProxy = SensorProxy.SensorProxy(temperatureSensor,1)
+# SensorProxies (Initialize with sensor)
+temperatureSensorProxy = SensorProxy.SensorProxy(temperatureSensor, 1)
 
-#Actuators
-#buzzerActuator=BuzzerActuator()
+# Actuators
+# buzzerActuator=BuzzerActuator()
 
-#Events (Initialize with actuators)
-highTemperatureEvent=HighTemperatureEvent.HighTemperatureEvent()
+# Events (Initialize with actuators)
+highTemperatureEvent = HighTemperatureEvent.HighTemperatureEvent()
 
-#Observers (Initialize with proxies they subscribe to and events that should be raised)
-highTemperatureObserver=HighTemperatureObserver.HighTemperatureObserver([temperatureSensorProxy],[highTemperatureEvent])
+# Observers (Initialize with proxies they subscribe to and events that should be raised)
+highTemperatureObserver = HighTemperatureObserver.HighTemperatureObserver([temperatureSensorProxy],
+                                                                          [highTemperatureEvent])
 
-#Add Observers
+# Add Observers
 temperatureSensorProxy.addObserver(highTemperatureObserver)
 
 temperatureSensorProxy.start()
