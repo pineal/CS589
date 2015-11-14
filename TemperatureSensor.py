ed9f1cc12f0676f3164d4ecc1876ba98dcc86617
Admin <Admin@Rasco-MacBook.local>
2015-11-13 17:56:09 -0800

    Added TemperatureSensor implementation

--- TemperatureSensor.py
+++ TemperatureSensor.py
@@ -1,11 +1,17 @@
+import pyupm_grove as grove
 from AbstractSensor import AbstractSensor
 
 class TemperatureSensor(AbstractSensor):
 
-	def __init__(self):
-		self.data=1
+	def __init__(self,pin,unit):
+		self.tempSensor = grove.GroveTemp(pin)
+		self.unit = unit
 		pass
 
 	def readData(self):
-		self.data=self.data+1
+		self.data = temp.value()
+	    
+	    if(self.unit=='F'):
+	    	fahrenheit = celsius * 9.0/5.0 + 32.0;
+	    	
 		return self.data
