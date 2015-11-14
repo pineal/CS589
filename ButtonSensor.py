980c530648d807e3cf5a5d2d0638e4693d1f09c1
Admin <Admin@Rasco-MacBook.local>
2015-11-13 17:27:56 -0800

    Fixed import line on ButtonSensor

--- ButtonSensor.py
+++ ButtonSensor.py
@@ -2,9 +2,8 @@ import pyump_grove as grove
 from AbstractSensor import AbstractSensor
 
 class ButtonSensor(AbstractSensor):
-
 	def __init__(self,pin):
 		self.button=grove.GroveButton(pin)
 
 	def readData(self):
-            return self.button.value()
+		return self.button.value()
