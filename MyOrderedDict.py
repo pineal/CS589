d3f465dcbbaa7f7fe7c34776cabbaf037370309c
Admin <Admin@Rasco-MacBook.local>
2015-11-14 21:49:32 -0800

    Added debugging line

--- MyOrderedDict.py
+++ MyOrderedDict.py
@@ -1,11 +1,11 @@
 from collections import OrderedDict
 
 class MyOrderedDict(OrderedDict):
-	def next_key(self, key):
+	def nextKey(self, key):
 		next=self._OrderedDict__map[key][1]
 		if next is self._OrderedDict__root:
-			return self.first_key()
+			return self.firstKey()
 		return next[2]
-	def first_key(self):
+	def firstKey(self):
 		for key in self: return key
 		raise ValueError("OrderedDict() is empty")
