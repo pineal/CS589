05916690239bf4459f25a0341d364b26f62e1ebb
Admin <Admin@Rasco-MacBook.local>
2015-11-15 12:31:18 -0800

    Fixed holding button behaviour

--- ButtonPressedObserver.py
+++ ButtonPressedObserver.py
@@ -5,10 +5,15 @@ class ButtonPressedObserver(AbstractObserver):
 	def __init__(self,buttonSensorProxy,events):
             super(ButtonPressedObserver,self).__init__(events)
             self.buttonSensorProxy=buttonSensorProxy
+            self.released=True
 
 	def notify(self):
             print "ButtonPressedObserver Notified"
-            if(self.buttonSensorProxy.data==1):
+            if(self.released==True and self.buttonSensorProxy.data==1):
+            	self.released=False
                 for e in self.events:
                     if(e.thrown==False):
                         e.throw()
+
+            elif(self.buttonSensorProxy.data==0):
+            	self.released=True
