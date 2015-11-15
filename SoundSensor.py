9bcca4db83f8e1106e4521b2a88165ff5b1567ff
Admin <Admin@Rasco-MacBook.local>
2015-11-15 15:43:20 -0800

    Fix var name

--- SoundSensor.py
+++ SoundSensor.py
@@ -5,13 +5,17 @@ class SoundSensor(AbstractSensor):
 
 	def __init__(self,pin):
 		self.soundSensor = upmMicrophone.Microphone(pin)
+		self.threshContext = upmMicrophone.thresholdContext()
+		self.threshContext.averageReading=0
+		self.threshContext.runningAverage=0
+		self.threshContext.averagedOver=2
 
 	def readData(self):
 		while(1):
 			buffer = upmMicrophone.uint16Array(128)
 			len = self.soundSensor.getSampledWindow(2, 128, buffer);
 			if len:
-				thresh = self.soundSensor.findThreshold(threshContext, 30, buffer, len)
-				self.soundSensor.printGraph(threshContext)
+				thresh = self.soundSensor.findThreshold(self.threshContext, 30, buffer, len)
+				self.soundSensor.printGraph(self.threshContext)
 				if(thresh):
 					return thresh
