2b24098e93638738f7e092cdca56bb594d3207b7
Admin <Admin@Rasco-MacBook.local>
2015-11-15 12:55:04 -0800

    Added hardware implementation of buzzer

--- BuzzerActuator.py
+++ BuzzerActuator.py
@@ -1,6 +1,11 @@
 from AbstractActuator import AbstractActuator
+import pyupm_buzzer as upmBuzzer
 
 class BuzzerActuator(AbstractActuator):
 
+	def __init__(object,pin):
+		self.buzzer=upmBuzzer(pin)
+
 	def trigger(self):
 		print "Bzzzzzzzz"
+		self.buzzer.playSound(upmBuzzer.DO)
--- client.py
+++ client.py
@@ -24,7 +24,7 @@ temperatureSensorProxy = SensorProxy(temperatureSensor, 2)
 buttonSensorProxy = SensorProxy(buttonSensor, 0.2)
 
 # Actuators
-buzzerActuator=BuzzerActuator()
+buzzerActuator=BuzzerActuator(2)
 toggleLcdDisplayMenuActuator=ToggleLcdDisplayMenuActuator(lcdDisplay)
 
 # Events (Initialize with actuators)
