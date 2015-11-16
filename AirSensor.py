8d201282ac7aca63c2e5d7a792a16a8a8f7b6309
Admin <Admin@Rasco-MacBook.local>
2015-11-15 19:47:02 -0800

    Added debug lines

--- AirSensor.py
+++ AirSensor.py
@@ -9,6 +9,6 @@ class AirSensor(AbstractSensor):
 	def readData(self):
 		self.sample=self.airSensor.getSample()
 		self.data = self.airSensor.getPPM()
-		print "PPM: " + self.data
-		print "Raw: " + self.sample
+		print "PPM: " + str(self.data)
+		print "Raw: " + str(self.sample)
 		return self.data
