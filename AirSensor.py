99f1a3486043dea98dfa2f7be897e95bb6913d2a
Admin <Admin@Rasco-MacBook.local>
2015-11-15 20:23:05 -0800

    Added smoothing

--- AirSensor.py
+++ AirSensor.py
@@ -7,11 +7,11 @@ class AirSensor(AbstractSensor):
 	def __init__(self,pin,precision):
 		self.airSensor = TP401.TP401(pin)
 		self.precision = precision
-		self.readings=deque(5*[0],5)
+		self.readings=deque(10*[0],10)
 
 	def readData(self):
 
 		self.readings.append(self.airSensor.getPPM())
-		self.data = sum(self.readings)/float(5)
-		
+		self.data = sum(self.readings)/float(10)
+
 		return round(self.data, self.precision)
