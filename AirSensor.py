62db95c0a4372c61f6f1082b68468209ee98548f
Admin <Admin@Rasco-MacBook.local>
2015-11-15 20:38:48 -0800

    Added smoothing to SensorProxy class

--- AirSensor.py
+++ AirSensor.py
@@ -3,12 +3,9 @@ from AbstractSensor import AbstractSensor
 
 class AirSensor(AbstractSensor):
 
-	def __init__(self,pin,precision,smoothing=1):
+	def __init__(self,pin,precision):
 		self.airSensor = TP401.TP401(pin)
 		self.precision = precision
-
-		self.smoothing=smoothing
-		self.readings=deque(self.smoothing*[0],self.smoothing)
 
 	def readData(self):
 
