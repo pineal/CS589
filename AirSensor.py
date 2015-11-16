588a6b5f80a955b072838967f417a6891921a28a
Admin <Admin@Rasco-MacBook.local>
2015-11-15 20:21:54 -0800

    Added smoothing

--- AirSensor.py
+++ AirSensor.py
@@ -1,12 +1,17 @@
 import pyupm_gas as TP401
 from AbstractSensor import AbstractSensor
+from collections import deque
 
 class AirSensor(AbstractSensor):
 
 	def __init__(self,pin,precision):
 		self.airSensor = TP401.TP401(pin)
 		self.precision = precision
+		self.readings=deque(5*[0],5)
 
 	def readData(self):
-		self.data = self.airSensor.getPPM()
+
+		self.readings.append(self.airSensor.getPPM())
+		self.data = sum(self.readings)/float(5)
+		
 		return round(self.data, self.precision)
