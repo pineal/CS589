686176f8ea3a32c90f54a1e61909b6897f772dca
Admin <Admin@Rasco-MacBook.local>
2015-12-07 11:37:01 -0800

    Fixed implementation for DummySensor

--- TimeObserver.py
+++ TimeObserver.py
@@ -3,8 +3,8 @@ from AbstractObserver import AbstractObserver
 class TimeObserver(AbstractObserver):
 
 	def __init__(self,events):
-            super(CloudObserver,self).__init__(events)
+		super(CloudObserver,self).__init__(events)
 
 	def notify(self):
-        for e in self.events:
-            e.throw()
+		for e in self.events:
+			e.throw()
