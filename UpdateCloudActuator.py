fc01d7ea81025831d9480f7bf51dab8509e0fbe3
Admin <Admin@Rasco-MacBook.local>
2015-12-07 12:03:08 -0800

    Updated values

--- UpdateCloudActuator.py
+++ UpdateCloudActuator.py
@@ -6,6 +6,7 @@ class UpdateCloudActuator(AbstractActuator):
 	def __init__(self,cloudUrl,sensorProxies):
 		self.cloudUrl=cloudUrl
 		self.sensorProxies=sensorProxies
+		self.payload={}
 
 	def trigger(self):
 		print "Update request sent to the cloud"
